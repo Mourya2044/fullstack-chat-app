15d0e1ce360e5aaeae1b3f1b6fee0306817ecdc4
Mourya <108566691+Mourya2044@users.noreply.github.com>
2025-06-13 19:56:53 +0530

    Project Complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
+
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore.js'
+import { useThemeStore } from './store/useThemeStore.js'
+
+import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
+
+const App = () => {
+
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  console.log('onlineUsers:', onlineUsers); 
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
+
+  console.log('authUser:', authUser);
+  if(isCheckingAuth && !authUser) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <Loader className="size-10 animate-spin"/>
+      </div>
+    )
+  }
+
+  return (
+    <div data-theme={theme}>
+
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path='/settings' element={<SettingsPage />} />
+      </Routes>
+      
+
+      <Toaster />
+    </div>
+  )
+}
+
+export default App
